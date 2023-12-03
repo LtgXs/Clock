0a958e4b4b061063fd6aa52003c3d1bdd18bb6cb
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-12-03 14:49:21 +0800

    Add files via upload

---
+++ wallpaper.py
@@ -0,0 +1,38 @@
+import requests
+import os
+
+
+api_url = "https://www.bing.com/HPImageArchive.aspx?n=1&mkt=zh-cn&idx=0&format=js"
+response = requests.get(api_url)
+if response.status_code == 200:
+    
+    data = response.json()
+    urlbase = data["images"][0]["urlbase"]
+    url = "https://www.bing.com" + urlbase + "_UHD.jpg"
+    image_data = requests.get(url).content
+    image_path = os.path.join("img", "background.jpg")
+    os.makedirs(os.path.dirname(image_path), exist_ok=True)
+    with open(image_path, "wb") as f:
+        f.write(image_data)
+        f.close()
+    print(f"图片已经保存到 {image_path}")
+
+    enddate = data["images"][0]["enddate"]
+    yy = f"{enddate[:4]}"
+    mm = f"{enddate[4:6]}"
+    dd = f"{enddate[6:]}"
+    enddate = f"{enddate[:4]}-{enddate[4:6]}-{enddate[6:]}"
+
+    copy = data["images"][0]["copyright"]
+    title = data["images"][0]["title"]
+    info_path = os.path.join("img","info")
+    with open(info_path, "w", encoding='utf-8') as f:
+        f.write(f"## Today's Bing Wallpaper\n")
+        f.write(f"Title: {title}\n")
+        f.write(f"Update: {enddate}\n")
+        f.write(f"Copyright: {copy}\n")
+        f.write(f"\nAuto get programm by LtgX\n")
+    print(f"信息已经保存到 {info_path}")
+    
+else:
+    print(f"请求失败，状态码为 {response.status_code}")
